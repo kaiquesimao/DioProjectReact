d0d15a2c6ea6ee581dc447cbfe86b1d8362f65fb
kaiquesimao <kaique.gabriel.me@gmail.com>
2023-01-07 01:32:29 -0300

    Adicionado estilos globais

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import {
     createBrowserRouter,
     RouterProvider,
 } from "react-router-dom";
 
-import Home from './pages/home/home'
+import {GlobalStyle} from "./styles/global.js";
+import Home from './pages/home/home';
 import Login from "./pages/login/login.jsx";
 
 const router = createBrowserRouter([
@@ -19,6 +20,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+      <GlobalStyle />
       <RouterProvider router={router} />
   </React.StrictMode>,
 )
