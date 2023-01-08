d98be29b9d855d7b2ff4422f0ecfebc6fca9d16b
kaiquesimao <Kaique.gabriel.me@gmail.com>
2023-01-08 18:13:49 -0300

    font open sans em todo lugar

--- src/styles/global.js
+++ src/styles/global.js
@@ -4,9 +4,11 @@ export const GlobalStyle = createGlobalStyle`
     * {
         margin: 0;
         padding: 0;
+        font-family: 'Open Sans', sans-serif;
     }
     body {
       background-color: #1E192C;
       color: white;
+      font-family: 'Open Sans', sans-serif;
     }
 `
