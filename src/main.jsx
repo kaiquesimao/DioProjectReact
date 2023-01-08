55398d45569ed3843ddc23e81c8d7eb327c6a260
kaiquesimao <Kaique.gabriel.me@gmail.com>
2023-01-08 19:02:28 -0300

    Formatação do codigo dos arquivos principais de config

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import {GlobalStyle} from "./styles/global.js";
-import Home from './pages/home/home';
+import { GlobalStyle } from "./styles/global.js";
+import Home from "./pages/home/home";
 import Login from "./pages/login/login";
-import Feed from './pages/feed/feed';
+import Feed from "./pages/feed/feed";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home />,
-    }, 
-    {
-        path: "/login",
-        element: <Login />,
-    },
-    {
-        path: "/feed",
-        element: <Feed />,
-    },
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/feed",
+    element: <Feed />,
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <GlobalStyle />
-      <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <GlobalStyle />
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
