76693765872888e871f3f3a203d4648331140d33
kaiquesimao <Kaique.gabriel.me@gmail.com>
2023-01-08 17:09:58 -0300

    criação do componente userInfo no feed

--- src/pages/feed/feed.jsx
+++ src/pages/feed/feed.jsx
@@ -1,5 +1,6 @@
 import Card from "../../components/card/card.jsx";
 import { Header } from "../../components/header/header.jsx";
+import UserInfo from "../../components/userInfo/userInfo.jsx";
 
 import { Container, TextContent, Title, TitleHighlight } from "./styles.js";
 
@@ -9,6 +10,7 @@ const Feed = () => {
       <Header />
       <Container>
         <Card />
+        <UserInfo percentual={50} nome="Kaique Simão" image="https://avatars.githubusercontent.com/u/66140734?v=4" />
       </Container>
     </>
   );
